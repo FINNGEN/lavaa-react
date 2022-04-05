e331f5f7fed49e2f9141e370ea92c064c26d5c02
major seitan <majorseitan@gmail.com>
2022-04-05 17:14:48 -0400

    add type declarations

---
+++ index.d.ts
@@ -0,0 +1,29 @@
+export interface Phenotype {
+    af_alt:                  number;
+    af_alt_cases:            number;
+    af_alt_controls:         number;
+    beta:                    number;
+    category:                string;
+    category_index:          number;
+    gc_lambda:               { [key: string]: number };
+    mlogp:                   number;
+    n_het_cases:             number;
+    n_het_controls:          number;
+    n_hom_cases:             number;
+    n_hom_controls:          number;
+    n_hom_ref_cases:         number;
+    n_hom_ref_controls:      number;
+    num_cases:               number;
+    num_cases_prev:          'NA' | number;
+    num_controls:            number;
+    num_gw_significant:      number;
+    num_gw_significant_prev: 'NA' | number;
+    phenocode:               string;
+    phenostring:             string;
+    pval:                    number;
+    sebeta:                  number;
+}
+
+export type color_map = {[p: string]: string}
+export interface Props { colorByCategory : color_map , dataprop : Phenotype }
+export default function VariantLavaPlot(prop : Props): JSX.Element
