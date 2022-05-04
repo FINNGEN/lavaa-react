d033581fddd765bafd7a48ef2a8f30f29315f3d8
major seitan <majorseitan@gmail.com>
2022-05-04 17:14:16 -0400

    add sample project

--- index.d.ts
+++ index.d.ts
@@ -1,29 +1,37 @@
 export interface Phenotype {
-    af_alt:                  number;
-    af_alt_cases:            number;
-    af_alt_controls:         number;
-    beta:                    number;
-    category:                string;
-    category_index:          number;
-    gc_lambda:               { [key: string]: number };
-    mlogp:                   number;
-    n_het_cases:             number;
-    n_het_controls:          number;
-    n_hom_cases:             number;
-    n_hom_controls:          number;
-    n_hom_ref_cases:         number;
-    n_hom_ref_controls:      number;
-    num_cases:               number;
-    num_cases_prev:          'NA' | number;
-    num_controls:            number;
-    num_gw_significant:      number;
-    num_gw_significant_prev: 'NA' | number;
-    phenocode:               string;
-    phenostring:             string;
-    pval:                    number;
-    sebeta:                  number;
+
+    beta:              number | null;
+    category:          string;
+    category_index? :  number;
+
+    maf:               number | null;
+    af_alt?:           number | null;
+
+    matching_results?: MatchingResults;
+    mlogp?:            number | null;
+
+    n_case?:           number | null;
+    num_cases?:        number | null;
+    af_alt_cases?:     number | null;
+    maf_case?:         number | null;
+
+
+    n_control?:        number | null;
+    num_control?:      number | null;
+    af_alt_controls?:  number | null;
+    maf_alt_controls?: number | null;
+    maf_control?:      number | null;
+
+    n_sample?:         "NA" | number | string;
+    phenocode:         string;
+    phenostring:       string;
+    pval:              number | null;
+    pip?:              number | null;
+
 }
 
+export interface MatchingResults {}
+
 export type color_map = {[p: string]: string}
-export interface Props { colorByCategory : color_map , dataprop : Phenotype[] }
+export interface Props { colorByCategory? : color_map , dataprop : Phenotype[] }
 export function Lavaa(prop : Props): JSX.Element
---
+++ lavaa-app/README.md
@@ -0,0 +1,6 @@
+# setup
+```
+npm run install
+run run build
+run run start
+```
